761b38c4aa284150a42d964430c7c9b429498abf
meraihan <sayedmahmudraihan@gmail.com>
2019-05-11 10:10:05 -0700

    Bcryption encryption class added

--- src/main/java/com/brainstation/socialmedia/TravelWorld/utils/Helper.java
+++ src/main/java/com/brainstation/socialmedia/TravelWorld/utils/Helper.java
@@ -1,11 +1,16 @@
 package com.brainstation.socialmedia.TravelWorld.utils;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.sql.Timestamp;
 import java.time.*;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class Helper {
+
+    public static PasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
 
     public Timestamp timestamp = new Timestamp(System.currentTimeMillis());
     public static ZoneId defaultZoneId = TimeZone.getDefault().toZoneId();
