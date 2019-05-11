df32391564889b23461e556b45deacf016d1d703
meraihan <sayedmahmudraihan@gmail.com>
2019-05-11 10:07:30 -0700

    User Repository Added with required Class

---
+++ src/main/java/com/brainstation/socialmedia/TravelWorld/repository/UserRepository.java
@@ -0,0 +1,81 @@
+package com.brainstation.socialmedia.TravelWorld.repository;
+
+import com.brainstation.socialmedia.TravelWorld.model.User;
+import com.brainstation.socialmedia.TravelWorld.model.rowmapper.UserRowMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User findByUsername(String username) {
+        User user;
+        String query = "Select * from user where user_name like ? order by id desc limit 1";
+
+        try {
+            user = jdbcTemplate.queryForObject(query, new Object[] {username}, new UserRowMapper());
+            return user;
+        } catch (DataAccessException dae) {
+            log.error("An exception occurred when executing the following query:");
+            log.error(query.replace("?", username));
+            log.error(dae.getLocalizedMessage());
+        }
+        return null;
+    }
+
+
+    public List<User> findAll() {
+        List<User> users;
+        String query = "Select * from user";
+
+        try {
+            users = jdbcTemplate.query(query, new UserRowMapper());
+            return users;
+        } catch (DataAccessException dae) {
+            log.error("An exception occurred when executing the following query:");
+            log.error(query);
+            log.error(dae.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    public User add(User user) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("user")
+                .usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+
+        parameters.put("full_name", user.getFullName());
+        parameters.put("user_name", user.getUserName());
+        parameters.put("password", user.getPassword());
+        parameters.put("email", user.getEmail());
+        parameters.put("role", user.getRole());
+        parameters.put("age", user.getAge());
+        parameters.put("gender", user.getGender().name());
+        parameters.put("address", user.getAddress());
+        parameters.put("created_at", user.getCreatedAt());
+        parameters.put("updated_at", user.getUpdatedAt());
+        parameters.put("deleted_at", user.getDeletedAt());
+
+        Number id = simpleJdbcInsert.executeAndReturnKey(parameters);
+        if (id == null) {
+            log.error("Failed to insert {}", user);
+            return null;
+        }
+        user.setId(id.intValue());
+        return user;
+    }
+}
