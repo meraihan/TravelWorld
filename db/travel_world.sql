c3d3e10f810c0399c7d89cd3b49d319939a3d3ca
meraihan <sayedmahmudraihan@gmail.com>
2019-05-12 12:52:08 -0700

    db dump updated

--- db/travel_world.sql
+++ db/travel_world.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.7.26-0ubuntu0.16.04.1 : Database - travel_world
+MySQL - 5.5.5-10.1.13-MariaDB : Database - travel_world
 *********************************************************************
 */
 
@@ -31,7 +31,7 @@ CREATE TABLE `area` (
 
 /*Data for the table `area` */
 
-insert  into `area`(`id`,`location`,`created_at`,`updated_at`,`deleted_at`) values (1,'Dhaka','2019-05-12 08:20:00','2019-05-12 08:20:00','2019-05-12 08:20:00'),(2,'Khulna','2019-05-12 08:20:02','2019-05-12 08:20:02','2019-05-12 08:20:02'),(3,'Sylhet','2019-05-12 08:20:07','2019-05-12 08:20:07','2019-05-12 08:20:07');
+insert  into `area`(`id`,`location`,`created_at`,`updated_at`,`deleted_at`) values (1,'Khulna','2019-05-11 15:25:48','2019-05-11 15:25:48','2019-05-11 15:25:48'),(2,'Sylhet','2019-05-11 15:25:51','2019-05-11 15:25:51','2019-05-11 15:25:51'),(3,'Dhaka','2019-05-11 15:25:55','2019-05-11 15:25:55','2019-05-11 15:25:55');
 
 /*Table structure for table `posts` */
 
@@ -41,8 +41,8 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `post` varchar(255) NOT NULL,
   `status` enum('PUBLIC','PRIVATE') NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `location_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -51,11 +51,11 @@ CREATE TABLE `posts` (
   KEY `location_id` (`location_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `area` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `posts` */
 
-insert  into `posts`(`id`,`post`,`status`,`user_id`,`location_id`,`created_at`,`updated_at`,`deleted_at`) values (1,'aaaaaaaaa','PUBLIC',1,1,'2019-05-12 08:20:22','2019-05-12 08:20:22','2019-05-12 08:20:22'),(2,'bbbbbbbbbb','PRIVATE',1,2,'2019-05-12 08:20:33','2019-05-12 08:20:33','2019-05-12 08:20:33'),(3,'ccccccccccc','PUBLIC',2,3,'2019-05-12 08:20:48','2019-05-12 08:20:48','2019-05-12 08:20:48');
+insert  into `posts`(`id`,`post`,`status`,`user_id`,`location_id`,`created_at`,`updated_at`,`deleted_at`) values (1,'aaaaaaaaaaa','PRIVATE',1,3,'2019-05-11 15:26:12','2019-05-11 15:26:12','2019-05-11 15:26:12'),(3,'ccccccc','PUBLIC',1,3,'2019-05-11 15:26:52','2019-05-11 15:26:52','2019-05-11 15:26:52');
 
 /*Table structure for table `user` */
 
@@ -67,19 +67,20 @@ CREATE TABLE `user` (
   `user_name` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT NULL,
+  `role` enum('ADMIN','USER') DEFAULT NULL,
   `gender` enum('MALE','FEMALE','OTHER') DEFAULT NULL,
-  `is_active` tinyint(4) NOT NULL,
-  `role` varchar(32) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKob8kqyqqgmefl0aco34akdtpe` (`full_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `user` */
 
-insert  into `user`(`id`,`full_name`,`user_name`,`password`,`email`,`address`,`gender`,`is_active`,`role`,`created_at`,`deleted_at`,`updated_at`) values (1,'Sayed Mahmud','meraihan','$2a$10$P/7uBuitgfcpS2E4TdFSkOlQ5YSO0fiO.eVAh8hFmsC37dLHqkl2W','asdasd',NULL,'MALE',1,'USER','2019-05-12 08:10:07','2019-05-12 08:10:10','2019-05-12 08:10:13'),(2,'Iren Sultana','iren','$2a$10$P/7uBuitgfcpS2E4TdFSkOlQ5YSO0fiO.eVAh8hFmsC37dLHqkl2W','asd',NULL,'MALE',1,'USER','2019-05-12 08:42:37','2019-05-12 08:42:39','2019-05-12 08:42:40'),(3,'Sayed Mahmud Raihan','aaa','$2a$10$1zNfThxIiMI5pXPoZ4xiT.4echCsuqY8AMfD0KHxGkPvy0lr2C3je','ponyirenpony@gmail.com',NULL,'MALE',1,'USER','2019-05-12 10:13:43','2019-05-12 10:13:45','2019-05-12 10:13:47'),(4,'Sayed Mahmud Raihan','raihan','$2a$10$fNWQK5FQLJ0nTClLbgKPj.mHujmY431BB9Bs6myH2rb8PiOYPbCOy','ponyirenpony@gmail.com',NULL,'FEMALE',1,'USER','2019-05-12 10:13:49','2019-05-12 10:13:51','2019-05-12 10:13:52');
+insert  into `user`(`id`,`full_name`,`user_name`,`password`,`email`,`is_active`,`role`,`gender`,`address`,`created_at`,`updated_at`,`deleted_at`) values (1,'Sayed Mahmud','meraihan','$2a$10$P/7uBuitgfcpS2E4TdFSkOlQ5YSO0fiO.eVAh8hFmsC37dLHqkl2W','a@gmail.com',NULL,'USER','MALE','aaaaa','2019-05-11 10:53:32','2019-05-11 10:53:32','2019-05-11 10:53:32'),(2,'Iren Sultana','iren','$2a$10$HGCtS/1g7OdD.YmDEUst3euJoNIOgCbRMBIIwrkITt7jtnCfqXdsG','a@gmail.com',NULL,'USER','FEMALE',NULL,'2019-05-11 11:30:54','2019-05-11 11:30:57','2019-05-11 11:31:00');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
